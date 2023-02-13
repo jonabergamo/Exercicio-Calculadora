15f4bd6c6047a10a8a08ff885eb5a00c810d4aea
Jonathan Oliveira Bergamo <jonathanbergamo16@gmail.com>
2023-02-13 19:55:38 -0300

    Co-authored-by: Gabriel Ramos  <Gabriel-R4@users.noreply.github.com> Co-authored-by: GtOliv3r <GtOliv3r@users.noreply.github.com> Co-authored-by: camiyuka <camiyuka@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-var salario = Number.FloatParse(document.getElementById("#salBruto"))
-var dep = Number.FloatParse(document.getElementById("#dependentes"))
-var i_nss = 0
-var i_nssRed = 0
+console.log(salario, dep);
 
-// Calculo de INSS 
+function calcular() {
+  var salBruto = document.querySelector("#salBruto");
+  var dependentes = document.querySelector("#dependentes");
+  var checkvale = document.querySelector("#caixinha");
+  var caixinha = checkvale.checked;
+  var salario = salBruto.value;
+  var dep = dependentes.value;
+  var i_nssRed = "";
+  var i_nss = 0;
+  document.querySelector(".ContainerOutput").style.opacity = "1";
 
-if(salario<=1302.00){
-    i_nss  = salario * 0.075
+  if (caixinha) {
+    document.querySelector("#reVale").innerHTML =
+      "R$" + parseFloat(salario * 0.06).toFixed(2);
+  } else {
+    document.querySelector("#reVale").innerHTML = "none";
+  }
+
+  // Calculo de INSS
+  if (salario <= 1302.0) {
+    i_nss = salario * 0.075;
     i_nssRed = parseFloat(i_nss.toFixed(2));
-    }
-    
-    else if(salario>=1302.01 && salario<=2571.29){
-    i_nss  = salario * 0.09
+  } else if (salario >= 1302.01 && salario <= 2571.29) {
+    i_nss = salario * 0.09;
     i_nssRed = parseFloat(i_nss.toFixed(2));
-    }
-    
-    else if(salario>=2571.30 && salario<=3856.94){
-    i_nss  = salario * 0.12
+  } else if (salario >= 2571.3 && salario <= 3856.94) {
+    i_nss = salario * 0.12;
     i_nssRed = parseFloat(i_nss.toFixed(2));
-    }
+  } else if (salario >= 3856.95 && salario <= 7507.49) {
+    i_nss = salario * 0.14;
+    i_nssRed = parseFloat(i_nss.toFixed(2));
+  } else {
+    i_nss = 7507.5 * 0.14;
+    i_nssRed = parseFloat(i_nss.toFixed(2));
+  }
+
+  if (reLiquido){
     
-    else if(salario>=3856.95 && salario<=7507.49){
-        i_nss = salario * 0.14
-        i_nssRed = parseFloat(i_nss.toFixed(2));
-    }
-    
-    else{
-        i_nss = 7507.50 * 0.14
-        i_nssRed = parseFloat(i_nss.toFixed(2));
-    }
+  }
 
-    //PUXAR i_nssRed por que ela é o valor arredondado
+  document.querySelector("#reInss").innerHTML = "R$" + i_nssRed;
+}
 
+function resetar() {
+  document.querySelector("#reInss").innerHTML = "";
+  document.querySelector(".ContainerOutput").style.opacity = "0";
+}
 
-    
-
-
-
+//PUXAR i_nssRed por que ela é o valor arredondado
