d6b370e50ac748f87beca6a85b8a60a8df8f38d0
Jonathan Oliveira Bergamo <jonathanbergamo16@gmail.com>
2023-02-13 22:48:19 -0300

    Co-authored-by: Gabriel Ramos  <Gabriel-R4@users.noreply.github.com> Co-authored-by: camiyuka <camiyuka@users.noreply.github.com> Co-authored-by: GtOliv3r <GtOliv3r@users.noreply.github.com>

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function calcular() {
   var dep = dependentes.value;
   var i_nss = "";
   document.querySelector(".ContainerOutput").style.opacity = "1";
-  document.querySelector("#reSalBruto").innerHTML = salario;
+  document.querySelector("#reSalBruto").innerHTML = parseFloat(salario).toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
 
   //salario bruto
 
@@ -23,11 +23,11 @@ function calcular() {
   } else {
     i_nss = 7507.5 * 0.14;
   }
-  document.querySelector("#reInss").innerHTML = parseFloat(i_nss).toFixed(2);
+  document.querySelector("#reInss").innerHTML = parseFloat(i_nss).toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
   
   //vale Transporte
   if (caixinha) {
-    var reVale = parseFloat(salario * 0.06).toFixed(2);
+    var reVale = parseFloat(salario * 0.06).toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
   } else {
     var reVale = 0;
   }
@@ -41,47 +41,47 @@ function calcular() {
     document.querySelector("#reVale").style.display = "none";
   }
 
-  //salário liquido
-  document.querySelector("#reLiquido").innerHTML = parseFloat(
-    salario - i_nss - reVale // tem que colocar - irrf ainda
-  ).toFixed(2);
+ 
 
   // IRRF
   var salarioBase = (salario - i_nss) - (189.59 * dep)
   console.log("Salario Base é: " + salarioBase)
 
-  if (salarioBase <= 1903.98) {
-    i_rff=0
+  if (salarioBase <= 1903.98) {  //funciona 
+    i_rrf=0
   }
 
-  else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) { //funciona
     i_rrf = ((salarioBase * 0.075) - 142.8)
   }
 
-  else if (salarioBase >= 2826.66 && salarioBase <= 3751.05)
+  else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) //funciona
     {
     i_rrf = ((salarioBase*0.15)-354.80)
     }
 
-    else if(salarioBase>=3751.06 && salarioBase<=4664.68)
+    else if(salarioBase>=3751.06 && salarioBase<=4664.68) //funciona 
     {
         i_rrf = ((salarioBase*0.225)-636.13)
     }
 
     else 
     {
-            i_rrf = ((salarioBase*0.275)-869.36)
+            i_rrf = ((salarioBase*0.275)-869.36) //funciona 
 
   }
   if (i_rrf >= 0) {
-    document.querySelector("#reIrrf").innerHTML = parseFloat(i_rrf).toFixed(2);
+    document.querySelector("#reIrrf").innerHTML = parseFloat(i_rrf).toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
   } 
   
   else 
   {
     document.querySelector("#reIrrf").innerHTML = 0
   }
-  
+   //salário liquido
+   document.querySelector("#reLiquido").innerHTML = parseFloat(
+    salario - i_nss - reVale - parseFloat(i_rrf)
+  ).toLocaleString('pt-BR',{style:'currency',currency:'BRL'});
 }
 
 function resetar() {
